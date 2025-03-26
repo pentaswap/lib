027cc9310cc08f3e817a1dcfb60a5d372f40cb79
Syed Mohib Uddin <36672373+syedMohib44@users.noreply.github.com>
2025-03-26 16:24:47 +0700

    Update TransferHelper.sol

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.6.0;
 
-// helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
+// helper methods for interacting with ERC20 tokens and sending PEN that do not consistently return true/false
 library TransferHelper {
     function safeApprove(
         address token,
@@ -12,7 +12,7 @@ library TransferHelper {
         // bytes4(keccak256(bytes('approve(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
         require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
+            success && (data.length == 0  abi.decode(data, (bool))),
             'TransferHelper::safeApprove: approve failed'
         );
     }
@@ -25,7 +25,7 @@ library TransferHelper {
         // bytes4(keccak256(bytes('transfer(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
         require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
+            success && (data.length == 0  abi.decode(data, (bool))),
             'TransferHelper::safeTransfer: transfer failed'
         );
     }
@@ -44,8 +44,8 @@ library TransferHelper {
         );
     }
 
-    function safeTransferETH(address to, uint256 value) internal {
+    function safeTransferPEN(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
+        require(success, 'TransferHelper::safeTransferPEN: PEN transfer failed');
     }
 }
